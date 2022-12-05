2be7d9700f08941222a4a276937c056eddc416e8
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2022-12-05 15:13:11 +0000

    feat: Make app a service with API

--- core/app.js
+++ core/app.js
@@ -1,9 +1,11 @@
-import { listUsers, getUserInfo, setUserToken } from "./database.js";
+import express from "express";
+
+import { listUsers, getUserInfo } from "./database.js";
 import generateToken from "./generate-token.js";
 import updateAvailability from "./update-availability.js";
 import updateIcs from "./update-ics.js";
 
-async function main() {
+async function updateAll() {
     const users = await listUsers();
     users.map(async (user) => {
         const webid = user.webid;
@@ -16,6 +18,49 @@ async function main() {
             console.warn(`No issuer field for ${webid}. Skipping`);
         }
     });
+    return users;
 }
 
-main();
+const app = express();
+const port = 3000;
+const update_interval = 4 * 60 * 60 * 1000; // 4 hours
+
+app.use(express.json()); // for parsing application/json
+
+app.get('/', (req, res) => {
+    res.send("The calendar orchestrator is running!");
+});
+
+app.post('/user', async (req, res) => {
+    const req_content = req.body;
+    const webid = req_content['webid'];
+    const issuer = req_content['issuer'];
+    const email = req_content['email'];
+    const password = req_content['password'];
+    const cal_url = req_content['cal_url'];
+    const res1 = await generateToken(email, password, webid, issuer);
+    if (!res1) {
+        res.status(401);
+        res.json({ error: "Could not generate token" });
+        return;
+    }
+    const res2 = await updateIcs(cal_url, webid);
+    if (!res2) {
+        res.status(401);
+        res.json({ error: "Could not set calendar URL" });
+        return;
+    }
+    res.send(res2);
+});
+
+app.get('/status', async (req, res) => {
+    const result = await updateAll();
+    res.send(result);
+});
+
+app.listen(port, () => {
+    console.log(`Orchestrator app listening on port ${port}`);
+});
+
+updateAll();
+setInterval(updateAll, update_interval);
