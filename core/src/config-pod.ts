8f249ec4b3c37310fb8041c9eb701130685808b4
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2023-01-26 21:36:17 +0000

    style: Use four spaces for indent

--- core/src/config-pod.ts
+++ core/src/config-pod.ts
@@ -7,7 +7,7 @@ import { getSolidDataset, saveSolidDatasetAt, deleteSolidDataset, getThingAll, g
 import { universalAccess } from "@inrupt/solid-client";
 
 export interface Config {
-	cal: string,
+    cal: string,
 }
 
 const N = 'urn:cal:orchestrator#';
@@ -18,66 +18,66 @@ const P_A = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type';
 const CONF_THING = 'config';
 
 function configLocation(pod: string) {
-	return `${pod}calendar_orchestrator/config.ttl`;
+    return `${pod}calendar_orchestrator/config.ttl`;
 }
 
 export async function retrieveConfig(pod: string, authFetch) {
-	const myDataset = await getSolidDataset(
-		configLocation(pod),
-		{ fetch: authFetch }
-	);
-	const things = getThingAll(myDataset);
-	let cal_url;
-	for (const thing of things) {
-		const cal = getStringNoLocale(thing, P_CAL)
-		if (cal) {
-			cal_url = cal;
-			break;
-		}
-	}
-	return {cal: cal_url};
+    const myDataset = await getSolidDataset(
+        configLocation(pod),
+        { fetch: authFetch }
+    );
+    const things = getThingAll(myDataset);
+    let cal_url;
+    for (const thing of things) {
+        const cal = getStringNoLocale(thing, P_CAL)
+        if (cal) {
+            cal_url = cal;
+            break;
+        }
+    }
+    return {cal: cal_url};
 }
 
 export async function updateConfig(pod: string, config: Config, authFetch, create?: boolean) {
-	let myDataset, configThing;
-	const save = async () => {
-		return await saveSolidDatasetAt(
-			configLocation(pod),
-			myDataset,
-			{ fetch: authFetch }
-		);
-	}
-	if (create) {
-		myDataset = createSolidDataset();
-		myDataset = await save();
-		// setConfigPermission(pod, authFetch);
-		configThing = buildThing(createThing({name: CONF_THING}))
-				.setUrl(P_A, C_CONF)
-				.build();
-	} else {
-		myDataset = await getSolidDataset(configLocation(pod), { fetch: authFetch });
-		configThing = getThing(myDataset, `${configLocation(pod)}#${CONF_THING}`);
-	}
-	configThing = buildThing(configThing)
-		.setStringNoLocale(P_CAL, config.cal)
-		.build();
-	myDataset = setThing(myDataset, configThing);
-	myDataset = await save();
-	return myDataset;
+    let myDataset, configThing;
+    const save = async () => {
+        return await saveSolidDatasetAt(
+            configLocation(pod),
+            myDataset,
+            { fetch: authFetch }
+        );
+    }
+    if (create) {
+        myDataset = createSolidDataset();
+        myDataset = await save();
+        // setConfigPermission(pod, authFetch);
+        configThing = buildThing(createThing({name: CONF_THING}))
+                .setUrl(P_A, C_CONF)
+                .build();
+    } else {
+        myDataset = await getSolidDataset(configLocation(pod), { fetch: authFetch });
+        configThing = getThing(myDataset, `${configLocation(pod)}#${CONF_THING}`);
+    }
+    configThing = buildThing(configThing)
+        .setStringNoLocale(P_CAL, config.cal)
+        .build();
+    myDataset = setThing(myDataset, configThing);
+    myDataset = await save();
+    return myDataset;
 }
 
 // Note: Not working, due to https://github.com/inrupt/solid-client-js/issues/1549
 // export async function setConfigPermission(pod: string, authFetch) {
-// 	await universalAccess.setPublicAccess(
-// 		configLocation(pod),
-// 		{ read: false, write: false },
-// 		{ fetch: authFetch }
-// 	);
+//     await universalAccess.setPublicAccess(
+//         configLocation(pod),
+//         { read: false, write: false },
+//         { fetch: authFetch }
+//     );
 // }
 
 export async function deleteConfig(pod: string, authFetch) {
-	return await deleteSolidDataset(
-		configLocation(pod),
-		{ fetch: authFetch }           // fetch function from authenticated session
-	);
+    return await deleteSolidDataset(
+        configLocation(pod),
+        { fetch: authFetch }           // fetch function from authenticated session
+    );
 }
--- core/src/database.ts
+++ core/src/database.ts
@@ -11,126 +11,126 @@ const DB_PATH = "db.json";
 let db_parsed = {};
 
 function reloadDB() {
-  db_parsed = JSON.parse(fs.readFileSync(DB_PATH).toString());
+    db_parsed = JSON.parse(fs.readFileSync(DB_PATH).toString());
 }
 
 function writeDB() {
-  fs.writeFileSync(DB_PATH, JSON.stringify(db_parsed));
+    fs.writeFileSync(DB_PATH, JSON.stringify(db_parsed));
 }
 
 if (fs.existsSync(DB_PATH)) {
-  reloadDB();
+    reloadDB();
 } else {
-  writeDB();
+    writeDB();
 }
 
 export interface UserInfo {
-  issuer: string,
-  id: string,
-  secret: string,
-  url?: string | null,
+    issuer: string,
+    id: string,
+    secret: string,
+    url?: string | null,
 }
 
 export async function setUserToken(webid: string, issuer: string, id: string, secret: string) {
-  db_parsed[webid] = {
-    issuer: issuer,
-    token_id: id,
-    token_secret: secret,
-  };
-  writeDB();
-
-  return true;
-}
-
-export async function setCalendarSourceUrl(webid: string, ics: string) {
-  if (webid in db_parsed) {
-    const userInfo = await getUserInfo(webid, true);
-    if (userInfo) {
-      const { id, secret, issuer, url } = userInfo;
-      let authFetch = await getAuthFetch(id, secret, issuer);
-      const pod = (await getPodUrlAll(webid))[0];
-      const conf = {cal: ics};
-      await updateConfig(pod, conf, authFetch, url == null);
-      return true;
-    }
-  }
-  return false;
-}
-
-export async function deleteUser(webid: string) {
-  if (webid in db_parsed) {
-    const userInfo = await getUserInfo(webid, true);
-    if (userInfo) {
-      const { id, secret, issuer, url } = userInfo;
-      if (url) {
-        let authFetch = await getAuthFetch(id, secret, issuer);
-        const pod = (await getPodUrlAll(webid))[0];
-        try {
-          await deleteConfig(pod, authFetch); //TODO: What if failed? Needs to notify the user.
-        } catch(e) {
-          return false;
-        }
-      }
-    }
-    delete db_parsed[webid];
+    db_parsed[webid] = {
+        issuer: issuer,
+        token_id: id,
+        token_secret: secret,
+    };
     writeDB();
 
     return true;
-  } else {
+}
+
+export async function setCalendarSourceUrl(webid: string, ics: string) {
+    if (webid in db_parsed) {
+        const userInfo = await getUserInfo(webid, true);
+        if (userInfo) {
+            const { id, secret, issuer, url } = userInfo;
+            let authFetch = await getAuthFetch(id, secret, issuer);
+            const pod = (await getPodUrlAll(webid))[0];
+            const conf = {cal: ics};
+            await updateConfig(pod, conf, authFetch, url == null);
+            return true;
+        }
+    }
     return false;
-  }
+}
+
+export async function deleteUser(webid: string) {
+    if (webid in db_parsed) {
+        const userInfo = await getUserInfo(webid, true);
+        if (userInfo) {
+            const { id, secret, issuer, url } = userInfo;
+            if (url) {
+                let authFetch = await getAuthFetch(id, secret, issuer);
+                const pod = (await getPodUrlAll(webid))[0];
+                try {
+                    await deleteConfig(pod, authFetch); //TODO: What if failed? Needs to notify the user.
+                } catch(e) {
+                    return false;
+                }
+            }
+        }
+        delete db_parsed[webid];
+        writeDB();
+
+        return true;
+    } else {
+        return false;
+    }
 }
 
 export async function getUserInfo(webid: string, includeConfig?: boolean): Promise<UserInfo | null> {
-  if (webid in db_parsed) {
-    const record = db_parsed[webid];
-    let cal_url: string | null | undefined = undefined;
-    if (includeConfig) {
-      let authFetch = await getAuthFetch(record.token_id, record.token_secret, record.issuer);
-      const pod = (await getPodUrlAll(webid))[0];
-      await retrieveConfig(pod, authFetch)
-      .then((conf) => {
-        cal_url = conf.cal;
-      })
-      .catch((e) => {
-        console.error(`Failed to retrieve config in Pod... Likely it does not exist`);
-        cal_url = null;
-      })
+    if (webid in db_parsed) {
+        const record = db_parsed[webid];
+        let cal_url: string | null | undefined = undefined;
+        if (includeConfig) {
+            let authFetch = await getAuthFetch(record.token_id, record.token_secret, record.issuer);
+            const pod = (await getPodUrlAll(webid))[0];
+            await retrieveConfig(pod, authFetch)
+            .then((conf) => {
+                cal_url = conf.cal;
+            })
+            .catch((e) => {
+                console.error(`Failed to retrieve config in Pod... Likely it does not exist`);
+                cal_url = null;
+            })
+        }
+        return {
+            issuer: record.issuer,
+            id: record.token_id,
+            secret: record.token_secret,
+            url: cal_url,
+        }
+    } else {
+        return null;
     }
-    return {
-      issuer: record.issuer,
-      id: record.token_id,
-      secret: record.token_secret,
-      url: cal_url,
-    }
-  } else {
-    return null;
-  }
 }
 
 export async function userInfoState(webid: string) {
-  const result = await getUserInfo(webid, true);
+    const result = await getUserInfo(webid, true);
 
-  if (result == null) {
+    if (result == null) {
+        return {
+            user: false,
+            ics: false,
+        };
+    }
+
+    if (result.url == null) {
+        return {
+            user: true,
+            ics: false,
+        };
+    }
+
     return {
-      user: false,
-      ics: false,
-    };
-  }
-
-  if (result.url == null) {
-    return {
-      user: true,
-      ics: false,
-    };
-  }
-
-  return {
-    user: true,
-    ics: true,
-  }
+        user: true,
+        ics: true,
+    }
 }
 
 export async function listUsers() {
-  return Object.keys(db_parsed).map(webid => { return {webid: webid} });
+    return Object.keys(db_parsed).map(webid => { return {webid: webid} });
 }
