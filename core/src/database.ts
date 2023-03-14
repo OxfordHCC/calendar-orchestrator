8689e5378748dffef31f41d6f266a73e9324eadc
Rui Zhao (renyuneyun) <renyuneyun@gmail.com>
2023-03-14 15:53:49 +0000

    docs/core: More comments and TODOs for database.ts

--- core/src/database.ts
+++ core/src/database.ts
@@ -24,6 +24,13 @@ if (fs.existsSync(DB_PATH)) {
     writeDB();
 }
 
+/**
+ * The representation of user information retrieved from the (orchestrator's) database.
+ * It (should) only have the entries that exist in the database.
+ * But right now, for compatibility, it may also have the calendar URL(s). This will be removed in the future.
+ * TODO: (After that of `getUserInfo()`) Remove URLs of the calendars.
+ * TODO: Store URL for calendar configuration.
+ */
 export interface UserInfo {
     issuer: string,
     id: string,
@@ -31,6 +38,16 @@ export interface UserInfo {
     url?: string[] | null,
 }
 
+/**
+ * Register the user and store the access token for the user.
+ * The access token is based on that for CSS (Community Solid Server). Other Solid server implementations may have their own mechanism that is not yet supported.
+ * TODO: Support all Solid services. Maybe through using WebID for the orchestrator.
+ * @param webid The WebID of the user.
+ * @param issuer The issuer of this WebID.
+ * @param id The token ID obtained from the Solid service (compatible with CSS).
+ * @param secret The token secret obtain from the Solid service (compatible with CSS).
+ * @returns `true` if no error is raised.
+ */
 export async function setUserToken(webid: string, issuer: string, id: string, secret: string) {
     db_parsed[webid] = {
         issuer: issuer,
@@ -42,6 +59,14 @@ export async function setUserToken(webid: string, issuer: string, id: string, se
     return true;
 }
 
+/**
+ * Set the external calendar URL(s) for the given user (WebID).
+ * This is for compatibility. The orchestrator should not directly modify the configuration. User the App should be preferred.
+ * TODO: (After that of `getUserInfo()`) Remove this function.
+ * @param webid The WebID of the user, whose calendar URLs are to be modified/replaced.
+ * @param ics The new calendar URL(s) (string or string array).
+ * @returns `true` if success; `false` if user not registered.
+ */
 export async function setCalendarSourceUrl(webid: string, ics: string[]|string) {
     if (webid in db_parsed) {
         const userInfo = await getUserInfo(webid, true);
@@ -60,6 +85,12 @@ export async function setCalendarSourceUrl(webid: string, ics: string[]|string) 
     return false;
 }
 
+/**
+ * De-register the user (WebID) from the orchestrator.
+ * TODO: Handle different status differently, which may require changing return values.
+ * @param webid The WebID to de-register.
+ * @returns `true` if success; `false` if deleting failed or user does not exist (not registered).
+ */
 export async function deleteUser(webid: string) {
     if (webid in db_parsed) {
         const userInfo = await getUserInfo(webid, true);
@@ -84,6 +115,14 @@ export async function deleteUser(webid: string) {
     }
 }
 
+/**
+ * Get the information for the given user (WebID) that is stored in the orchestrator.
+ * For compatibility, it also supports including the calendar URL(s) by passing `true` to the `includeConfig` parameter.
+ * TODO: (After that from `userInfoState()`) Stop getting the configuration, and remove the `includeConfig` parameter.
+ * @param webid The WebID of the user to see information.
+ * @param includeConfig Whether to include the configuration content (i.e. the calendar URLs) or not.
+ * @returns The user information, or `null` indicating user does not exist.
+ */
 export async function getUserInfo(webid: string, includeConfig?: boolean): Promise<UserInfo | null> {
     if (webid in db_parsed) {
         const record = db_parsed[webid];
@@ -112,6 +151,12 @@ export async function getUserInfo(webid: string, includeConfig?: boolean): Promi
     }
 }
 
+/**
+ * Tell whether some information (or the user) exists for the user or not.
+ * TODO: Remove `ics` from return.
+ * @param webid The WebID of the user.
+ * @returns Whether certain information exist for the user or not, as an object. Currently supports `user` for existence of the user (i.e. registered or not), and `ics` for configuration existence.
+ */
 export async function userInfoState(webid: string) {
     const result = await getUserInfo(webid, true);
 
@@ -135,6 +180,11 @@ export async function userInfoState(webid: string) {
     }
 }
 
+/**
+ * List all registered users with the orchestrator.
+ * TODO: Maybe return array of webids?
+ * @returns The list of users, represented as `{webid: WEBID}` where `WEBID` is the actual WebID (string).
+ */
 export async function listUsers() {
     return Object.keys(db_parsed).map(webid => { return {webid: webid} });
 }
